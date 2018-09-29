68adc455a58a188cced680b47a145c47d00687aa
juliof9 <00081817@uca.edu.sv>
2018-09-28 23:30:16 -0600

    TErminando

---
+++ Ejercicio_12.js
@@ -0,0 +1,27 @@
+while (flag){
+    var option = prompt("La jefecita: \n 1.Agg")
+    switch(option){
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        case "5":
+            break;
+        case "6":
+            alert("Adios gfecita :`)")
+            flag = false;
+            break;
+        case null:
+            alert("Hasta la vista gfa");
+            flag = false;
+            break;
+        default:
+            alert("Escoja una opcion valida: ");
+            break;
+    }
+}
+
