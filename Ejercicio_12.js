0a2f7021ee8e14b92eda302ec14400e2239b2a55
juliof9 <00081817@uca.edu.sv>
2018-09-28 23:52:26 -0600

    Terminando casi

--- Ejercicio_12.js
+++ Ejercicio_12.js
@@ -1,7 +1,16 @@
 while (flag){
+    articulos = [];
     var option = prompt("La jefecita: \n 1.Agg")
     switch(option){
         case "1":
+            id = prompt("Agrege el codigo del articulo")
+                r = prompt("Descrpcion del articulo?")
+                type = prompt("Tipo de articulo?")
+                purchasePrice = parseFloat(prompt("Precio de compra?", "0.00"));
+                saleP = parseFloat(prompt("Inserte precio de la venta", "0.00"));
+                stock = parseInt(prompt("Inserte el stock del articulo", "0"));
+                var articulo = {id, r, type, purchasePrice, saleP, stock};
+                articulos.push(articulo);
             break;
         case "2":
             break;
@@ -10,6 +19,9 @@ while (flag){
         case "4":
             break;
         case "5":
+            articulos.forEach(element => {
+                console.log('Articulo: ' + element.r + 'Stock: ' + element.stock)
+            });
             break;
         case "6":
             alert("Adios gfecita :`)")
